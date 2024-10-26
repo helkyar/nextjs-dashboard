8719ca5014d918f4871819df3efdb10ad6d02efd
helkyar <javierpalaciosbotejara@gmail.com>
2024-10-26 11:55:03 +0200

    fix ts unregistered types

--- app/ui/invoices/breadcrumbs.tsx
+++ app/ui/invoices/breadcrumbs.tsx
@@ -1,17 +1,18 @@
-import { clsx } from 'clsx';
-import Link from 'next/link';
-import { lusitana } from '@/app/ui/fonts';
+import { clsx } from 'clsx'
+import Link from 'next/link'
+import { lusitana } from '@/app/ui/fonts'
+import { UrlObject } from 'url'
 
 interface Breadcrumb {
-  label: string;
-  href: string;
-  active?: boolean;
+  label: string
+  href: string
+  active?: boolean
 }
 
 export default function Breadcrumbs({
   breadcrumbs,
 }: {
-  breadcrumbs: Breadcrumb[];
+  breadcrumbs: Breadcrumb[]
 }) {
   return (
     <nav aria-label="Breadcrumb" className="mb-6 block">
@@ -21,10 +22,12 @@ export default function Breadcrumbs({
             key={breadcrumb.href}
             aria-current={breadcrumb.active}
             className={clsx(
-              breadcrumb.active ? 'text-gray-900' : 'text-gray-500',
+              breadcrumb.active ? 'text-gray-900' : 'text-gray-500'
             )}
           >
-            <Link href={breadcrumb.href}>{breadcrumb.label}</Link>
+            <Link href={breadcrumb.href as unknown as UrlObject}>
+              {breadcrumb.label}
+            </Link>
             {index < breadcrumbs.length - 1 ? (
               <span className="mx-3 inline-block">/</span>
             ) : null}
@@ -32,5 +35,5 @@ export default function Breadcrumbs({
         ))}
       </ol>
     </nav>
-  );
+  )
 }
--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -1,9 +1,10 @@
-'use client';
+'use client'
 
-import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
-import clsx from 'clsx';
-import Link from 'next/link';
-import { generatePagination } from '@/app/lib/utils';
+import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline'
+import clsx from 'clsx'
+import Link from 'next/link'
+import { generatePagination } from '@/app/lib/utils'
+import { UrlObject } from 'url'
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
   // NOTE: Uncomment this code in Chapter 11
@@ -49,7 +50,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
         />
       </div> */}
     </>
-  );
+  )
 }
 
 function PaginationNumber({
@@ -58,10 +59,10 @@ function PaginationNumber({
   isActive,
   position,
 }: {
-  page: number | string;
-  href: string;
-  position?: 'first' | 'last' | 'middle' | 'single';
-  isActive: boolean;
+  page: number | string
+  href: string
+  position?: 'first' | 'last' | 'middle' | 'single'
+  isActive: boolean
 }) {
   const className = clsx(
     'flex h-10 w-10 items-center justify-center text-sm border',
@@ -71,16 +72,16 @@ function PaginationNumber({
       'z-10 bg-blue-600 border-blue-600 text-white': isActive,
       'hover:bg-gray-100': !isActive && position !== 'middle',
       'text-gray-300': position === 'middle',
-    },
-  );
+    }
+  )
 
   return isActive || position === 'middle' ? (
     <div className={className}>{page}</div>
   ) : (
-    <Link href={href} className={className}>
+    <Link href={href as unknown as UrlObject} className={className}>
       {page}
     </Link>
-  );
+  )
 }
 
 function PaginationArrow({
@@ -88,9 +89,9 @@ function PaginationArrow({
   direction,
   isDisabled,
 }: {
-  href: string;
-  direction: 'left' | 'right';
-  isDisabled?: boolean;
+  href: string
+  direction: 'left' | 'right'
+  isDisabled?: boolean
 }) {
   const className = clsx(
     'flex h-10 w-10 items-center justify-center rounded-md border',
@@ -99,21 +100,21 @@ function PaginationArrow({
       'hover:bg-gray-100': !isDisabled,
       'mr-2 md:mr-4': direction === 'left',
       'ml-2 md:ml-4': direction === 'right',
-    },
-  );
+    }
+  )
 
   const icon =
     direction === 'left' ? (
       <ArrowLeftIcon className="w-4" />
     ) : (
       <ArrowRightIcon className="w-4" />
-    );
+    )
 
   return isDisabled ? (
     <div className={className}>{icon}</div>
   ) : (
-    <Link className={className} href={href}>
+    <Link className={className} href={href as unknown as UrlObject}>
       {icon}
     </Link>
-  );
+  )
 }
