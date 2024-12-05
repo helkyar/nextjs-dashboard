1ea42cca723b1a5b1251324b3bafc1b437fb79e0
helkyar <javierpalaciosbotejara@gmail.com>
2024-12-05 23:33:32 +0100

    fix customer crud

--- app/dashboard/customers/_lib/actions.ts
+++ app/dashboard/customers/_lib/actions.ts
@@ -1,12 +1,12 @@
 'use server'
 import { isAllowed } from '@/auth/auth-provider'
-import { validateFormData } from '@/lib/schema-validation'
+import { schema, validateFormData } from '@/lib/schema-validation'
 import { sql } from '@/lib/db-connection'
 import { revalidatePath } from 'next/cache'
 import { CostumerSchema } from '@/app/dashboard/customers/_lib/schemas'
 import fs from 'fs/promises'
-
-const customerTable = 'customers'
+import { randomUUID } from 'crypto'
+import { CustomerForm } from '@/lib/definitions'
 
 export type State = {
   errors?: {
@@ -38,20 +38,24 @@ export async function createCustomer(
       `public${imageUrl}`,
       new Uint8Array(await image.arrayBuffer())
     )
+
     await sql`
-  INSERT INTO ${customerTable} (name, email, image_url)
-  VALUES (${customerName}, ${email}, ${imageUrl})
+  INSERT INTO customers (id, name, email, image_url)
+  VALUES (${randomUUID()}, ${customerName}, ${email}, ${imageUrl})
 `
     return { success: 'Customer created successfully.' }
-  } catch {
+  } catch (e) {
+    console.log('🚀 ~ e:', e)
     return { error: 'Database Error: Failed to Create Customer.' }
   } finally {
     revalidatePath('/dashboard/customers')
   }
 }
 
-const UpdateCustomerSchema = CostumerSchema.omit({ id: true }).partial({
-  image: true,
+const UpdateCustomerSchema = CostumerSchema.omit({ id: true }).extend({
+  image: schema.instanceof(File, {
+    message: 'Please select an image',
+  }),
 })
 export async function updateCustomer(
   formData: FormData,
@@ -67,14 +71,29 @@ export async function updateCustomer(
   const { customerName, email, image } = validatedFields.data
 
   try {
-    if (image) {
+    if (image && image.size > 0) {
+      try {
+        const data = await sql<CustomerForm>`
+        SELECT
+          image_url
+        FROM customers
+        WHERE id = ${id}
+      `
+
+        const customer = data.rows[0]
+        await fs.unlink(`public${customer.image_url}`)
+      } catch (e) {
+        console.log('🚀 ~ e:', e)
+      }
+
       const imageUrl = `/customers/${image.name}`
       await fs.writeFile(
         `public${imageUrl}`,
         new Uint8Array(await image.arrayBuffer())
       )
+
       await sql`
-      UPDATE ${customerTable}
+      UPDATE customers
       SET name = ${customerName}, 
       email = ${email}, 
       image_url = ${imageUrl}
@@ -82,15 +101,16 @@ export async function updateCustomer(
     `
     } else {
       await sql`
-      UPDATE ${customerTable}
+      UPDATE customers
       SET name = ${customerName}, 
-      email = ${email}, 
+      email = ${email}
       WHERE id = ${id}
       `
     }
 
     return { success: 'Customer updated successfully.' }
-  } catch {
+  } catch (e) {
+    console.log('🚀 ~ e:', e)
     return { error: 'Customer update failed.' }
   } finally {
     revalidatePath('/dashboard/customers')
@@ -100,8 +120,18 @@ export async function updateCustomer(
 export async function deleteCustomer(id: string) {
   if (!(await isAllowed())) return { error: 'Log in to delete a Customer.' }
   try {
+    const data = await sql<CustomerForm>`
+      SELECT
+        image_url
+      FROM customers
+      WHERE id = ${id}
+    `
+
+    const customer = data.rows[0]
+    await fs.unlink(`public${customer.image_url}`)
+
     await sql`
-    DELETE FROM ${customerTable}
+    DELETE FROM customers
     WHERE id = ${id}
     `
     return { success: 'Customer deleted successfully.' }
