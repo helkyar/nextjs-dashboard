fdfeedd6af672ffa2b209e0868936f60f3b0dc9d
helkyar <javierpalaciosbotejara@gmail.com>
2024-12-07 20:05:22 +0100

    fix validation issue

--- lib/definitions.ts
+++ lib/definitions.ts
@@ -3,12 +3,12 @@
 // For simplicity of teaching, we're manually defining these types.
 // However, these types are generated automatically if you're using an ORM such as Prisma.
 export type User = {
-  id: number
-  username: string
+  id: string
+  name: string
   email: string
   password: string
-  token: string
   verified: boolean
+  token: string
   created_at: string
 }
 
